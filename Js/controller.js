5858c433be29fa5e74488ce32ac61cf25eebcebb
Rajat Juneja <32433607+Rajat-Juneja@users.noreply.github.com>
2018-03-31 13:30:27 +0530

    Add files via upload

--- Js/controller.js
+++ Js/controller.js
@@ -12,29 +12,29 @@ app.controller('myCtrl', function($scope) {
 
     $scope.function = (ev)=>{
 
-         
+        //  console.log(ev.srcElement.childNodes[1]);
         
     
         if(ev.keyCode==37) // left
        {    
         $scope.src="left.png";
         x-=0.5;
-        if(ev.srcElement.childNodes[3].x<width){
+        if(ev.srcElement.childNodes[1].x<width){
             x+=0.48;
             
         }
         
-        if(ev.srcElement.childNodes[3].x<minx){
+        if(ev.srcElement.childNodes[1].x<minx){
         width-=2;
         height-=1;
         }
-        if(ev.srcElement.childNodes[3].x>maxx){
+        if(ev.srcElement.childNodes[1].x>maxx){
         width+=2;
         height+=1;
         }
-        ev.srcElement.childNodes[3].style.width = width+"px";
-        ev.srcElement.childNodes[3].style.height = height+"px";
-        ev.srcElement.childNodes[3].style.left = x+"%";
+        ev.srcElement.childNodes[1].style.width = width+"px";
+        ev.srcElement.childNodes[1].style.height = height+"px";
+        ev.srcElement.childNodes[1].style.left = x+"%";
        }
        
        // console.log("yes");
@@ -45,49 +45,49 @@ app.controller('myCtrl', function($scope) {
         $scope.src="back.png";
         y-=0.5;
         // console.log(y);
-        if(ev.srcElement.childNodes[3].y<height+30){
+        if(ev.srcElement.childNodes[1].y<height+30){
             y+=0.49;
           
            
         }
 
        
-        if(ev.srcElement.childNodes[3].y<miny){
+        if(ev.srcElement.childNodes[1].y<miny){
         width-=1;
         height-=0.5;
         }
-        if(ev.srcElement.childNodes[3].y>maxy){
+        if(ev.srcElement.childNodes[1].y>maxy){
         width-=1;
         height-=0.5;
         }
        
         
-        ev.srcElement.childNodes[3].style.width = width+"px";
-        ev.srcElement.childNodes[3].style.height = height+"px";
+        ev.srcElement.childNodes[1].style.width = width+"px";
+        ev.srcElement.childNodes[1].style.height = height+"px";
         
-        ev.srcElement.childNodes[3].style.top = y+"%";
+        ev.srcElement.childNodes[1].style.top = y+"%";
        }
 
        else if(ev.keyCode==39) //right
        {
         $scope.src="right.png";
         x+=0.5;
-        if(ev.srcElement.childNodes[3].x>window.innerWidth-width){
+        if(ev.srcElement.childNodes[1].x>window.innerWidth-width){
             x-=0.48;
             
         }
         
-        if(ev.srcElement.childNodes[3].x<minx){
+        if(ev.srcElement.childNodes[1].x<minx){
         width+=2;
         height+=1;
         }
-        if(ev.srcElement.childNodes[3].x>maxx){
+        if(ev.srcElement.childNodes[1].x>maxx){
         width-=2;
         height-=1;
         }
-        ev.srcElement.childNodes[3].style.width = width+"px";
-        ev.srcElement.childNodes[3].style.height = height+"px";
-        ev.srcElement.childNodes[3].style.left = x+"%";
+        ev.srcElement.childNodes[1].style.width = width+"px";
+        ev.srcElement.childNodes[1].style.height = height+"px";
+        ev.srcElement.childNodes[1].style.left = x+"%";
        }
 
        else if(ev.keyCode=40) //down
@@ -95,22 +95,22 @@ app.controller('myCtrl', function($scope) {
         $scope.src="front.png";
         y+=0.5;
         // console.log("y is",y+3.4*height);
-        if(ev.srcElement.childNodes[3].y+height>window.innerHeight){
+        if(ev.srcElement.childNodes[1].y+height>window.innerHeight){
             y-=0.49;
             // console.log(window.innerHeight-20);
 
         }
-        if(ev.srcElement.childNodes[3].y<miny){
+        if(ev.srcElement.childNodes[1].y<miny){
         width+=1;
         height+=0.5;
         }
-        if(ev.srcElement.childNodes[3].y>maxy){
+        if(ev.srcElement.childNodes[1].y>maxy){
         width+=1;
         height+=0.5;
         }
-        ev.srcElement.childNodes[3].style.width = width+"px";
-        ev.srcElement.childNodes[3].style.height = height+"px";
-        ev.srcElement.childNodes[3].style.top = y+"%";
+        ev.srcElement.childNodes[1].style.width = width+"px";
+        ev.srcElement.childNodes[1].style.height = height+"px";
+        ev.srcElement.childNodes[1].style.top = y+"%";
        }
 
 
